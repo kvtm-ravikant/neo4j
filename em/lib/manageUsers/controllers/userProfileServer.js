99a900da218c0fd19ea87c5981a8e517f7de53fe
ravikant <ravikant@kvantuminc.com>
2014-06-23 21:43:15 +0530

    New changelist

--- em/lib/manageUsers/controllers/userProfileServer.js
+++ em/lib/manageUsers/controllers/userProfileServer.js
@@ -1,7 +1,8 @@
 /**
  * Created by ravikant on 20/6/14.
  */
-
+var location=require("../../common/models/location.js");
+var religion=require("../../common/models/religion.js")
 module.exports=function(app,Utils){
     app.get("/manage-users/user-profile",function(req,res){
         console.log("Inside /manage-users/create-user");
@@ -11,13 +12,29 @@ module.exports=function(app,Utils){
     app.get("/manage-users/getUserprofileForUserName",Utils.ensureAuthenticated,function(req,res){
         console.log("/manage-users/getUserprofileForUserName");
         try{
-            var userDetails=req.session.userDetails;
-            var responseObj=new Utils.Response();
-            responseObj.responseData=userDetails;
-            res.json(responseObj);
+
         }catch(e){
-            Utils.defaultErrorResponse(res,"Unable to get user details."+e)
+            console.log("execption",e);
+            Utils.defaultErrorResponse(res,"Unable to get user details."+e);
         }
+        var userDetails=req.session.userDetails;
+        var clonedUserDetails=Utils.clone(userDetails);
+        //resolve birth country
+        console.log(clonedUserDetails.basicDetails.birthPlace_country,location);
+        clonedUserDetails.basicDetails.birthPlace_country=location.countries[clonedUserDetails.basicDetails.birthPlace_country.toString()].countryName;
+        //resolve birth state
+        //console.log("state",clonedUserDetails.basicDetails.birthPlace_state);
+        //clonedUserDetails.basicDetails.birthPlace_state=location.states[clonedUserDetails.basicDetails.birthPlace_state.toString].stateName;
+        //delete city as we don't have its resolver
+        delete clonedUserDetails.basicDetails.birthPlace_city;
+        delete clonedUserDetails.basicDetails.accessList;
+        //religion
+        clonedUserDetails.basicDetails.religionName=religion[clonedUserDetails.basicDetails.religionId];
+
+        //sending response
+        var responseObj=new Utils.Response();
+        responseObj.responseData=clonedUserDetails;
+        res.json(responseObj);
 
 
     });
