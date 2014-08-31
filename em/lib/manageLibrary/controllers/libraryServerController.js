e9ce5cb2c9a067ff5ae48b944498000aa050cf08
ravikant <ravikant@kvantuminc.com>
2014-08-31 16:19:41 +0530

    New changelist

--- em/lib/manageLibrary/controllers/libraryServerController.js
+++ em/lib/manageLibrary/controllers/libraryServerController.js
@@ -5,7 +5,7 @@ var fs=require("fs");
 var libraryMS=require('../models/library.js');
 var Utils=require("../../common/Utils/Utils.js");
 var neo4j=require("node-neo4j");
-var db=new neo4j("http://106.185.38.115:7474");
+var db=new neo4j("http://localhost:7474");
 var parentBookDD=require("../../common/models/docType.js");
 
 var acceptableDataTypes=["string","number","boolean","date"];
