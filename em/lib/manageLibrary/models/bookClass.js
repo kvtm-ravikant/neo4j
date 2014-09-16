a7e3030653a22a796f79a008da0084a4e57556f1
ravikant <ravikant@kvantuminc.com>
2014-09-17 00:04:59 +0530

    view user

---
+++ em/lib/manageLibrary/models/bookClass.js
@@ -0,0 +1,24 @@
+/**
+ * Created by ravikant on 16/9/14.
+ */
+
+
+function ParentBook(){
+    this.bindingType="";
+    this.bookTitle="";
+    this.categoryName="";
+    this.bibLevel="";
+    this.edition=0;
+    this.bookCopies=0;
+    this.language="";
+    this.publisher="";
+    this.isbn="";
+    this.coverImagePath="";
+    this.authorName="";
+    this.docType="";
+    this.softDelete="";
+
+}
+function ChildBook(){
+
+}
