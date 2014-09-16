14521110c14a638b41aad5fde9dd6c26a0185cdc
puneetcse09 <puneetcse09@gmail.com>
2014-09-16 20:44:54 +0530

    ss

---
+++ em/app/scripts/modules/schoolManagement/controlllers/schoolAdmin_schoolProfile.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ravikant on 12/7/14.
+ */
+educationMediaApp.controller('schoolAdmin_schoolProfile', function ($scope, $http,iconClassMapping) {
+  
+	$scope.text="Scholl Profile";
+});
