3c6a5931fcfe471d70233fb9667a01c4769d8eb4
ravikant <ravikant@kvantuminc.com>
2014-09-28 15:21:44 +0530

    bug fixes

--- em/lib/common/models/userType.js
+++ em/lib/common/models/userType.js
@@ -5,6 +5,6 @@ module.exports={
 		"2":{"___name___":"Teacher"},
 		"1":{"___name___":"Student"},
 		"3":{"___name___":"Parent"},
-		"4":{"___name___":"Staff"}, 
-		"5":{"___name___":"Other"} 
+		"4":{"___name___":"Staff"}
+		"5":{"___name___":"Principal"}
 	    }
